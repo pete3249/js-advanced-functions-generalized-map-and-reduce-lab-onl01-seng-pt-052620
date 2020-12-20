5dec689a6885798548fdecf03cc6856f3a42c049
pete3249 <katelynrpeterson16@gmail.com>
2020-12-20 11:00:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(src, cb) { 
+    let r = []
+    for (let i=0; i < src.length; i++) {
+        let theElement = src[i]
+        r.push(cb(theElement))
+    }
+    return r;    
+}
+
+function reduce(src, cb, startingPoint) {
+    let r = (!!startingPoint) ? startingPoint : src[0]
+    let i = (!!startingPoint) ? 0: 1
+    
+    for (; i < src.length; i++) {
+        r = cb(src[i], r)
+    }
+    return r
+}
+  
